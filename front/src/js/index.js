f6dcdf8ab14657b99cd713a0a7dd1f2962359608
zhangxiaochen <zhangxiaochen40@sohu.com>
2018-10-26 23:19:40 +0800

    轮波图样式

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,1 +1,25 @@
-console.assert("hello word");
+function Banner() {
+
+}
+
+Banner.prototype.run=function () {
+    var bannerUl = $("#banner-ul");
+    var index=0;
+    // bannerUl.animate({"left" : -798},500);
+    setInterval(function () {
+        if(index>=1)
+        {
+            index=0;
+        }
+        else {
+
+            index++;
+        }
+        bannerUl.animate({"left" : -798*index},500);
+    },2000);
+};
+
+$(function () {
+    var banner =new Banner();
+    banner.run()
+});
