32a8c0d2826299ae4586dbe3b08408722b8b9c33
zhangxiaochen <zhangxiaochen40@sohu.com>
2018-12-02 11:07:05 +0800

    开始作后台管理系统（cms）页面

--- apps/cms/urls.py
+++ apps/cms/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import cms_login, index
+from .views import cms_login, index, write_news, news_category
 
 app_name='cms'
 
 urlpatterns = [
     path('login/', cms_login, name='login'),
     path('index/', index, name='index'),
+    path('write_news/', write_news.as_view(), name='write_news'),
+    path('news_category/', news_category, name='news_category'),
 ]
--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import View
 
 
 def cms_login(request):
@@ -10,6 +11,15 @@ def cms_login(request):
     return render(request,'cms/login.html')
 
 
-@staff_member_required(login_url='index')
+# @staff_member_required(login_url='index')
 def index(request):
     return render(request,'cms/index.html')
+
+
+class write_news(View):
+    def get(self,request):
+        return render(request,'cms/write_news.html')
+
+
+def news_category(request):
+    return render(request,'cms/news-category.html')
