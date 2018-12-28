2a737c6ba1d826cb3a5824bc151f14132b44b852
zhangxiaochen <zhangxiaochen40@sohu.com>
2018-12-28 22:10:32 +0800

    时间选择器控件

---
+++ front/src/js/news_list.js
@@ -0,0 +1,35 @@
+
+function CMSNewsList() {
+
+}
+
+CMSNewsList.prototype.initDatePicker = function () {
+    var startPicker = $("#start-picker");
+    var endPicker = $("#end-picker");
+
+    var todayDate = new Date();
+    var todayStr = todayDate.getFullYear() + '/' + (todayDate.getMonth()+1) + '/' + todayDate.getDate();
+    var options = {
+        'showButtonPanel': true,
+        'format': 'yyyy/mm/dd',
+        'startDate': '2017/6/1',
+        'endDate': todayStr,
+        'language': 'zh-CN',
+        'todayBtn': 'linked',
+        'todayHighlight': true,
+        'clearBtn': true,
+        'autoclose': true
+    };
+    startPicker.datepicker(options);
+    endPicker.datepicker(options);
+};
+
+
+CMSNewsList.prototype.run = function () {
+    this.initDatePicker();
+};
+
+$(function () {
+    var newsList = new CMSNewsList();
+    newsList.run();
+});
